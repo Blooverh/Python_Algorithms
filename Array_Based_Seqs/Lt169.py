371ab3e0ec21e40443d8f7f45b686a0ec6005e53
Diogo Aires <63264160+Blooverh@users.noreply.github.com>
2023-05-08 23:44:31 -0500

    Update Lt169.py

--- Array_Based_Seqs/Lt169.py
+++ Array_Based_Seqs/Lt169.py
@@ -1,18 +1,26 @@
 from collections import UserList
 """EXTREMELY SLOW SOLUTION USING JUST AN EXTRA ARRAY AS MEMORY"""
+# def majorityElement(nums: UserList[int]) -> int:
+#     counter=[ i * 0 for i in range(10000)] #list that will keep count of occurrences
+#     print(counter)
+#     if len(nums) <1:
+#         return 0
+    
+#     for num in nums:
+#         counter[num]+=1
+    
+#     return counter.index(max(counter))
+
+# list=[6,5,5]
+
+# print(majorityElement(list))
+
+"""Fastest Solution """
 def majorityElement(nums: UserList[int]) -> int:
-    counter=[ i * 0 for i in range(10000)] #list that will keep count of occurrences
-    print(counter)
-    if len(nums) <1:
-        return 0
-    
-    for num in nums:
-        counter[num]+=1
-    
-    return counter.index(max(counter))
-
-list=[6,5,5]
-
-print(majorityElement(list))
-
-    
+        sol = None
+        cnt = 0
+        for i in nums:
+            if cnt == 0:
+                sol = i
+            cnt += (1 if i == sol else -1)
+        return sol
