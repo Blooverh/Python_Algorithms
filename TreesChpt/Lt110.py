21bb1a611c5359b823bdba93f6e82e9691b7cfc5
Diogo Aires <63264160+Blooverh@users.noreply.github.com>
2023-05-30 21:45:33 -0500

    Update Lt110.py

--- TreesChpt/Lt110.py
+++ TreesChpt/Lt110.py
@@ -18,11 +18,16 @@ class Solution:
         """ 
         if root == None:
             return True
-
+        """Call helper method to calculate the height of each side and calculate it based on which node does have an 
+        element"""
         l = self.depth(root.left)
         r = self.depth(root.right)
+        """If absolute value of the subtraction of the depth of each side is less than 2 and both 
+        nodes are not null return true, else if depth of onde side is bigger than the other by 2 
+        units return false"""
         return (abs(l-r) <2) and self.isBalanced(root.left) and self.isBalanced(root.right)
     
+    """Calculates depth of each side based on which node is not null"""
     def depth(self,node):
         if node == None: return 0 # if node is none return 0 meaning there is no child node 
         return max(self.depth(node.left),self.depth(node.right))+1 # increment based on which node has children
