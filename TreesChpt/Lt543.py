5b9dc08e380b7e894de47f0f08aa677851680f04
Diogo Aires <63264160+Blooverh@users.noreply.github.com>
2023-06-07 23:08:37 -0500

    Update Lt543.py

--- TreesChpt/Lt543.py
+++ TreesChpt/Lt543.py
@@ -1,15 +1,19 @@
 
 
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
 class Solution:
     def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
         # Implement depth
 	    def depth(node: Optional[TreeNode]) -> int:
 		    return 1 + max(depth(node.left), depth(node.right)) if node else 0
-	
+
 	    return depth(root.left) + depth(root.right)  # calculate diameter
