2dc521956173aa85e68f88a033d3fadcc23f7f7f
Diogo Aires <63264160+Blooverh@users.noreply.github.com>
2023-06-18 19:17:04 -0500

    Create Lt35.py

---
+++ LeetCode/Lt35.py
@@ -0,0 +1,26 @@
+"""Given a sorted array of distinct integers and a target value, return the index if the target is found. 
+If not, return the index where it would be if it were inserted in order."""
+
+"""THIS IS A TYPICAL BINARY SEARCH PROBLEM"""
+from collections import UserList
+class Solution:
+    def searchInsert(self, nums: UserList[int], target: int) -> int:
+
+        if len(nums) ==0:
+            return 0
+
+        low= 0
+        high= len(nums)
+
+        while low < high:
+            mid=(low + high) //2
+
+            if nums[mid] > target:
+                high =  mid - 1
+            elif nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                low = mid +1 
+        """In case element not in list with the srkink of the list, last element for search will be lowest
+        position the element could have been if it was in the list """
+        return low 
