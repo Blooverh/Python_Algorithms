43dd8d8d910dd2abf3db874becf40085ddab9221
Diogo Aires <63264160+Blooverh@users.noreply.github.com>
2023-06-19 21:36:33 -0500

    recrusion continue

--- Recursion/ReverseSeqRecursion.py
+++ Recursion/ReverseSeqRecursion.py
@@ -3,7 +3,7 @@ def reverse(S, start, stop):
     """Reverse elements in implicit slice S[start:stop]"""
     if start < stop-1:
         S[start] , S[stop-1] = S[stop-1], S[start]
-        reverse(S, start+1, stop-1)
+        reverse(S, start+1, stop-1) #we decrease and increase stop and start with the recursion so we can swap elements
 
     return S
 
--- Recursion/fileSystem.py
+++ Recursion/fileSystem.py
@@ -1,15 +1,16 @@
 import os 
-print(os.path.getsize(path))
-print(os.path.isdir(path))
-print(os.listdir(path))
+# print(os.path.getsize(path))
+# print(os.path.isdir(path))
+# print(os.listdir(path))
 
 def disk_usage(path):
-    total= os.path.getsize(path)
-    if os.path.isdir(path):
-        childpath=os.path.join(path,'fileSystem.py')
-        total += disk_usage(childpath)
+    total= os.path.getsize(path) #account for direct usage 
+    if os.path.isdir(path): #if this path is a directory
+        for filename in os.listdir(path): #then for each child
+            childpath=os.path.join(path,filename) #compose full path to children 
+            total += disk_usage(childpath) # add childs size to total
 
     print('{0:7}'.format(total), path)
     return total
 
-disk_usage(path)
+disk_usage('C:/Users/diogo/OneDrive/Desktop/Python_Algorithms/Recursion')
