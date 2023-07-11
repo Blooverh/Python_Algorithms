eb23bfb252954d38189d4a3704ae359b486a6af9
Diogo Aires <63264160+Blooverh@users.noreply.github.com>
2023-07-10 21:15:33 -0500

    Heap Learning

---
+++ PriorityQueueChpt/heapPQ.py
@@ -0,0 +1,80 @@
+import PriorityQueue
+
+class Heap(PriorityQueue):
+    """A min-oriented priority queue implemented with a binary heap"""
+
+    #Non public methods (behaviors)
+    def _parent(self, j):
+        return (j-1)//2
+    
+    def _left(self, j):
+        return 2 * j + 1
+    
+    def _right(self, j):
+        return 2 * j + 2
+    
+    def _has_left(self, j): #index beyond list length
+
+        return self._left(j) < len(self._data)
+    
+    def _has_right(self, j): #index beyond list length
+
+        return self._right(j) < len(self._data)
+    
+    def _swap(self, i, j ):
+        """Swap elements at indices i and j of array"""
+        self._data[i], self._data[j] = self._data[j], self._data[i]
+
+    def _upheap(self, j):
+        parent= self._parent(j)
+        if j > 0 and self._data[j] < self._data[parent]:
+            self._swap(j, parent)
+            self._upheap(parent) #recursion at position of parent
+
+    def _downheap(self, j):
+        if self._has_left(j):
+            left=self._left(j)
+            small_child=left #although right may be smaller
+
+            if self._has_right(j):
+                right=self._right(j)
+                if self._data[right] < self._data[left]:
+                    small_child = right
+            
+            if self._data[small_child] < self._data[j]:
+                self._swap(j, small_child)
+                self._downheap(small_child) #recursion at position of small child
+
+    ### Public Methods Behaviors ###
+
+    def __init__(self):
+        self._data=[]
+
+    def __len__(self):
+        return len(self._data)
+    
+    def add(self, key, value):
+        """Add key bvalue pair to priority queue """
+        self._data.append(self._Item(key,value))
+        self._upheap(len(self._data)-1) #up-heap newly added position
+
+    def min(self):
+        """Return but do not remove key-value paior from priority queue with minimum key
+        raise exception is empty"""
+        if self.is_empty():
+            raise Empty("Priority queue is empty")
+        
+        item=self._data[0]
+        return (item._key, item._value) 
+    
+    def remove_min(self):
+        """Remove and return (k,v) tuple with minimum key
+        raise empty exception if priority queue is empty"""
+
+        if self.is_empty():
+            raise Empty("Priority queue is empty")
+        self.swap(0, len(self._data)-1) #put minimum item at end of the list
+        item=self._data.pop() #remove it from the list 
+        self._downheap(0) #then fix new root
+
+        return (item._key, item._value)
