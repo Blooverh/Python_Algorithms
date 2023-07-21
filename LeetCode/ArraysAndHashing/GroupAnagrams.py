7485eb2c23c3438b385560ea095a16e526ad24ff
Diogo Aires <63264160+Blooverh@users.noreply.github.com>
2023-07-20 23:48:44 -0500

    leetcode

--- LeetCode/ArraysAndHashing/GroupAnagrams.py
+++ LeetCode/ArraysAndHashing/GroupAnagrams.py
@@ -5,21 +5,32 @@ You can return the answer in any order.
 An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, 
 typically using all the original letters exactly once.
 """
-# from collections import UserList
-# class Solution:
-#     def groupAnagrams(self, strs: UserList[str]) -> UserList[UserList[str]]:
+from collections import UserList
+class Solution:
+    def groupAnagrams(self, strs: UserList[str]) -> UserList[UserList[str]]:
 
+        hashmap = {}
         
+        for word in strs:
+            # we sort the word so we can append it as a key because all words that are anagrams when sorted are the same
+            sortedWords= ''.join(sorted(word)) 
 
+            if sortedWords not in hashmap:
+                hashmap[sortedWords] = [word] # value is a list that will contain the word
+            else:
+                hashmap[sortedWords].append(word) #if key exists we append the current word
+        
+        ans= [] # list that will contains all the lists of anagrams
 
-                       
-            
-            
-            
+        for value in hashmap.values():
+            ans.append(value)
+        
+        return ans
+ 
 
-# test=Solution()
+test=Solution()
 
-# list=["eat","tea","tan","ate","nat","bat"]
+list=["eat","tea","tan","ate","nat","bat"]
 
-# print(test.groupAnagrams(list))
+print(test.groupAnagrams(list))
                 
---
+++ LeetCode/ArraysAndHashing/LongestConsecutiveSeq.py
@@ -0,0 +1,42 @@
+from typing import List
+
+
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        """
+        sort array
+        variable counter to keep count of consecutive elements 
+        once nums[i+1] - nums[i] > 1 
+        append current counter to a list 
+        and reset count and continue the pass
+        until list is done
+        append the current counter 
+
+        return the max element in the list that contains the counters
+        """
+
+        #base cases
+        if len(nums) <= 1:
+            return len(nums)
+
+        sortedNums = sorted(nums)
+        print(sortedNums)
+        counter=1
+        counters=[]
+        for i in range(1 , len(sortedNums)):
+            if sortedNums[i] - sortedNums[i-1] == 1:
+                counter +=1
+            elif sortedNums[i] - sortedNums[i-1] <= 0:
+                counter = counter
+            else:
+                counters.append(counter)
+                counter=0
+
+        
+        counters.append(counter)
+        print(counters)
+        return max(counters)
+
+a =Solution()
+arr= [9,1,4,7,3,-1,0,5,8,-1,6]
+print(a.longestConsecutive(arr))
